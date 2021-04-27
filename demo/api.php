bf9c09ee8df404fc0f3ab0c919433efd1e38db03
dante <dantinr@gmail.com>
2021-04-27 21:44:39 +0800

    demo

---
+++ demo/api.php
@@ -0,0 +1,21 @@
+<?php
+
+	$arr = [
+		[
+			"name"  => "zhangsan11",
+			"age"   => 11,
+			"email" => "zhangsan11@qq.com"
+		],
+		[
+			"name"  => "zhangsan22",
+			"age"   => 22,
+			"email" => "zhangsan22@qq.com"
+		],
+		[
+			"name"  => "zhangsan33",
+			"age"   => 33,
+			"email" => "zhangsan33@qq.com"
+		]
+	];
+
+	echo json_encode($arr);
